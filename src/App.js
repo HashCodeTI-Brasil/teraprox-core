85e0af9d4cc3f50eff493a3aa824dcd5acbbbabb
Alexandre Nunes <hashcodeti@gmail.com>
2026-04-04 12:17:45 -0300

    fix: clear global error on route change to allow navigation
    Adds a useEffect to App.js that dispatches clearGlobalError whenever the location changes. This ensures that the ServerErrorScreen is dismissed when the user clicks on a menu item or navigates to another path.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { FederatedComponentHost } from './factories/FederatedComponentHOC';
 import MenuBar from './Components/Menu/MenuBar';
 import Login from './Screens/Login';
 import Home from './Screens/Home';
 import ServerErrorScreen from './Components/error-handling/ServerErrorScreen';
+import { clearGlobalError } from './Reducers/default-reducers/globalErrorReducer';
 import { allFederatedRoutes as sgpRoutes } from './models/federatedProcessoScreens';
 import { allFederatedRoutes as sgmRoutes } from './models/federatedManutencaoScreens';
 import { allFederatedRoutes as cadastroRoutes } from './models/federatedCadastroScreens';
@@ -15,60 +17,61 @@ const allFederatedRoutes = [...sgpRoutes, ...sgmRoutes, ...cadastroRoutes, ...so
 const homeRoute = '/home';
 
 const App = () => {
+    const dispatch = useDispatch();
+    const location = useLocation();
     const global = useSelector(state => state.global);
     const globalError = useSelector(state => state.errors);
     const isAuth = global?.isAuth;
+
+    React.useEffect(() => {
+        if (globalError) {
+            dispatch(clearGlobalError());
+        }
+    }, [location.pathname]);
 
     const isServerError = globalError && (
         globalError.status === undefined ||   // network error (sem resposta do servidor)
         (globalError.status >= 500 && globalError.status < 600)  // 5xx
     );
 
-    if (isServerError) {
-        return (
-            <div className="teraprox-shell">
-                {isAuth && <MenuBar />}
-                <div className={isAuth ? "container-fluid mt-4" : ""}>
-                    <ServerErrorScreen />
-                </div>
-            </div>
-        );
-    }
-
     return (
         <div className="teraprox-shell">
             {isAuth && <MenuBar />}
             <div className={isAuth ? "container-fluid mt-4" : ""}>
                 <main>
-                    <Routes>
-                        <Route path="/Login" element={<Login />} />
+                    {isServerError ? (
+                        <ServerErrorScreen />
+                    ) : (
+                        <Routes>
+                            <Route path="/Login" element={<Login />} />
 
-                        {!isAuth ? (
-                            <Route path="*" element={<Navigate to="/Login" replace />} />
-                        ) : (
-                            <>
-                                <Route path="/" element={<Navigate to={homeRoute} replace />} />
-                                <Route path={homeRoute} element={<Home />} />
+                            {!isAuth ? (
+                                <Route path="*" element={<Navigate to="/Login" replace />} />
+                            ) : (
+                                <>
+                                    <Route path="/" element={<Navigate to={homeRoute} replace />} />
+                                    <Route path={homeRoute} element={<Home />} />
 
-                                {allFederatedRoutes.map((screen) => (
-                                    <Route
-                                        key={screen.routePath}
-                                        path={screen.routePath}
-                                        element={
-                                            <FederatedComponentHost
-                                                key={screen.routePath}
-                                                modulePath={screen.modulePath}
-                                                context={screen.context}
-                                                hideFooter
-                                            />
-                                        }
-                                    />
-                                ))}
+                                    {allFederatedRoutes.map((screen) => (
+                                        <Route
+                                            key={screen.routePath}
+                                            path={screen.routePath}
+                                            element={
+                                                <FederatedComponentHost
+                                                    key={screen.routePath}
+                                                    modulePath={screen.modulePath}
+                                                    context={screen.context}
+                                                    hideFooter
+                                                />
+                                            }
+                                        />
+                                    ))}
 
-                                <Route path="*" element={<Navigate to={homeRoute} replace />} />
-                            </>
-                        )}
-                    </Routes>
+                                    <Route path="*" element={<Navigate to={homeRoute} replace />} />
+                                </>
+                            )}
+                        </Routes>
+                    )}
                 </main>
             </div>
         </div>
